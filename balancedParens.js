d0aed7dee55740fccb3b8df1e7015c885481c876
Greg Stickel <gregory.stickel@live.com>
2022-10-05 09:40:59 -0400

    Updated 9:40am

--- balancedParens.js
+++ balancedParens.js
@@ -1,27 +1,29 @@
 // Sample Strings
-let sample1 = "This ( is unbalanced."
-let sample2 = "(This (is (a) balanced) string.)"
-let sample3 = "This is () also ) unbalanced."
-let sample4 = "Balanced."
+let sample1 = "This ( is unbalanced.";
+let sample2 = "(This (is (a) balanced) string.)";
+let sample3 = "This is () also ) unbalanced.";
+let sample4 = "Balanced.";
 
 // Write your solution below:
-function balancedParens (str) {
-    let result = false;
-    let count = 0;
-    for (let i = 0; i < str.length; i++) {
-        if(str[i] === "(") {
-            count++
-        };
-        if(str[i] === ")") {
-            count++
-        }
-        console.log(count);
-         
+function balancedParens(str) {
+  let result = false;
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "(") {
+      count++;
     }
-    if (count % 2 === 0) {
-        result = true;
+    if (str[i] === ")") {
+      count++;
     }
-    return(result);
+  }
+  if (count % 2 === 0 && count > 0) {
+    result = true;
+  }
+  return result;
 }
 
+console.log(balancedParens(sample1));
 console.log(balancedParens(sample2));
+console.log(balancedParens(sample3));
+console.log(balancedParens(sample4));
+
